4a9789cdcf945efbb413076435be77478e2379eb
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-11-08 23:58:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 TOKEN = os.environ.get("TOKEN", "6483499681:AAHEGzswREgXraiX02MCrDq5VvGE_9AWOqU")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "RASKEJ-KXROAR-YRFJSX-SCMXXG-ARQ")
 REPO_BOT = os.environ.get("REPO_BOT", "https://t.me/Kyyomasaa")
-OWNER = os.environ.get("OWNER", "")
-BOT_NAME = os.environ.get("BOT_NAME", "")
+OWNER = os.environ.get("OWNER", "5308312798")
+BOT_NAME = os.environ.get("BOT_NAME", "redrenamerbot")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "Kyyomasaa")
