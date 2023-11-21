fc581862b60be43c07bd96b88ea60805bbd0827e
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-11-21 01:00:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN = os.environ.get("TOKEN", "")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "Q")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "FIJROI-HUFQMF-REBCXR-EYQJFC-ARQ")
 REPO_BOT = os.environ.get("REPO_BOT", "https://t.me/Kyyomasaa")
 OWNER = os.environ.get("OWNER", "5308312798")
 BOT_NAME = os.environ.get("BOT_NAME", "")
