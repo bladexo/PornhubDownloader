1c0ed3cca902d86c32847f56b3d4865154d93492
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-11-22 19:29:32 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN = os.environ.get("TOKEN", "6846998381:AAHbbWJl65RxjcXTG1PllyDK_eZg3CMzsII")
-ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "FIJROI-HUFQMF-REBCXR-EYQJFC-ARQ")
+X_API_KEY = os.environ.get("X_API_KEY", "FIJROI-HUFQMF-REBCXR-EYQJFC-ARQ")
 REPO_BOT = os.environ.get("REPO_BOT", "https://t.me/RedOfficiall")
 OWNER = os.environ.get("OWNER", "6434260634")
 BOT_NAME = os.environ.get("BOT_NAME", "Redrtybot")
